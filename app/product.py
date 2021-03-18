51df47daa88ef833754759a9a94b73d46bc4ef92
Riki Permana <arwahdevops@gmail.com>
2021-03-19 00:08:37 +0700

    Restructure jinja templating

--- app/product.py
+++ app/product.py
@@ -4,8 +4,9 @@ product = Blueprint('product', __name__)
 
 @product.route('/product')
 def product_index():
+    judul = "Produk"
     cur = mysql.connection.cursor()
     cur.execute("select * from product")
     data = cur.fetchall()
     cur.close()
-    return render_template('product.html', product=data)
+    return render_template('product.html', judul=judul, product=data)
