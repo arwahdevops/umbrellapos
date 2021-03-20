d5a0726bc5b6b9726eef190d5428626d90c722bd
Riki Permana <arwahdevops@gmail.com>
2021-03-20 12:26:46 +0700

    add forgot password backend

---
+++ app/forgot.py
@@ -0,0 +1,8 @@
+from flask import Flask, render_template, Blueprint
+from .extensions import mysql
+forgot = Blueprint('forgot', __name__)
+
+@forgot.route('/forgot')
+def forgot_index():
+
+    return render_template('forgot_password.html')
